c67179943576c18f9863aa96e8b7f0b5dcb01445
Steven <stevefaliszewski@gmail.com>
2024-10-25 00:23:51 -0700

    Initial experimentations with rigid body

---
+++ Assets/BoxScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+
+    public Rigidbody boxRigidBody;
+
+    public float walkingSpeed;
+    public float jumpingSpeed;
+
+    public float runningSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        // boxRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            boxRigidBody.linearVelocity = Vector3.up * jumpingSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            boxRigidBody.linearVelocity = transform.forward * walkingSpeed;
+        }
+
+
+
+    }
+}

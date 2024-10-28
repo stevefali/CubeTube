ac86f43e637b22093374dd0badda4d41c12b5e67
Steven <stevefaliszewski@gmail.com>
2024-10-28 09:41:36 -0700

    Create function for initializing empty floor array

--- Assets/ProceduralGen.cs
+++ Assets/ProceduralGen.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class ProceduralGen
 {
     private BlockPlacingScript placer;
+    private Vector3Int mapSize;
     public ProceduralGen(BlockPlacingScript placer, Vector3Int mapSize)
     {
         this.placer = placer;
+        this.mapSize = mapSize;
     }
 
 
@@ -20,5 +22,25 @@ public class ProceduralGen
         Vector3 pos = new(15f, 15f, 15f);
         placer.PlaceBlock(pos);
     }
+
+
+    // The floor array will be arranged as (z, y, x)
+    private int[,,] InitializeFloorArray(int width, int height, int length)
+    {
+        int[,,] floorArray = new int[length, height, width];
+
+        for (int z = 0; z < floorArray.GetUpperBound(0); z++)
+        {
+            for (int y = 0; y < floorArray.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x < floorArray.GetUpperBound(2); x++)
+                {
+                    floorArray[z, y, x] = 0;
+                }
+            }
+        }
+        return floorArray;
+    }
+
 
 }

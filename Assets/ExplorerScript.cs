5d03fa9cd1fe373292794f7f7f7987be07d8678c
Steven <stevefaliszewski@gmail.com>
2024-11-04 02:17:04 -0800

    Setup explorer object with rigidbody, control script, and add main camera as child

---
+++ Assets/ExplorerScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExplorerScript : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float lookSensitivity;
+    private Vector3 explorerMovement = Vector3.zero;
+    private Vector3 explorerRotation = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * moveSpeed * explorerMovement);
+        transform.Rotate(explorerRotation * lookSensitivity);
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        explorerMovement = new Vector3(moveInput.x, 0, moveInput.y);
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 lookInput = context.ReadValue<Vector2>();
+        explorerRotation = new Vector3(-lookInput.y, lookInput.x, 0);
+    }
+}
